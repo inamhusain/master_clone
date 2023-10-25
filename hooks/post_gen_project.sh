82eb524bcac9ab45ab2c165978c8d06a807cc079
inamh_webelight <inamh@webelight.co.in>
2023-10-25 17:03:31 +0530

    latest

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -31,30 +31,30 @@ update_project_permissions() {
     cd "{{ cookiecutter.repo_name }}"
 }
 
-attempt_to_launch_studio() {
-    if [[ -z ${CI+x} ]]; then
-        check_for_launch_flag
-    else
-        echo "Skipping the launching of Android Studio because we're building on CI..."
-    fi
-}
+# attempt_to_launch_studio() {
+#     if [[ -z ${CI+x} ]]; then
+#         check_for_launch_flag
+#     else
+#         echo "Skipping the launching of Android Studio because we're building on CI..."
+#     fi
+# }
 
-check_for_launch_flag() {
-    if [[ "{{ cookiecutter.launch_studio }}" = true ]]; then
-        launch_studio
-    else
-        echo "Skipping the launching of Android Studio..."
-    fi
-}
+# check_for_launch_flag() {
+#     if [[ "{{ cookiecutter.launch_studio }}" = true ]]; then
+#         launch_studio
+#     else
+#         echo "Skipping the launching of Android Studio..."
+#     fi
+# }
 
-launch_studio() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        /Applications/Android\ Studio.app/Contents/MacOS/studio .
-        echo "Android Studio should now be running, have fun with your new project!"
-    else
-        echo "Unsupported operating system: skipping the launching of Android Studio..."
-    fi                                                                      
-}
+# launch_studio() {
+#     if [[ "$OSTYPE" == "darwin"* ]]; then
+#         /Applications/Android\ Studio.app/Contents/MacOS/studio .
+#         echo "Android Studio should now be running, have fun with your new project!"
+#     else
+#         echo "Unsupported operating system: skipping the launching of Android Studio..."
+#     fi                                                                      
+# }
 
 check_theme_flag() {
   # Remove theme related file if theme is not enabled
