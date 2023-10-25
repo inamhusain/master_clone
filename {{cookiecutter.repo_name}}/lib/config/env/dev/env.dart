f2a6a1b3e03a53f386a05d0d9b2d6400296db0a8
inamh_webelight <inamh@webelight.co.in>
2023-10-25 19:03:10 +0530

    latest

--- {{cookiecutter.repo_name}}/lib/config/env/dev/env.dart
+++ {{cookiecutter.repo_name}}/lib/config/env/dev/env.dart
@@ -5,5 +5,5 @@ part 'env.g.dart';
 @Envied(path: '.env.development')
 abstract class Env {
   @EnviedField(varName: 'BASE_URL', obfuscate: false)
-  static const baseUrl = _Env.baseUrl;
+  static String baseUrl = _Env.baseUrl;
 }
