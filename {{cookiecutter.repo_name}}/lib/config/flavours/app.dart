4fc9a51fe99625747ed9d71fc91a4f2a084c455c
inamh_webelight <inamh@webelight.co.in>
2023-10-25 19:09:04 +0530

    latest

---
+++ {{cookiecutter.repo_name}}/lib/config/flavours/app.dart
@@ -0,0 +1,40 @@
+// // Flutter imports:
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// // Package imports:
+// import 'package:master_utility/master_utility.dart';
+
+// enum Environment { adminDev, adminProd, userDev, userProd }
+
+// class AppConfig {
+//   Future<void> setAppConfig({required Environment environment}) async {
+//     await _initializeApp(environment: environment);
+//     await PreferenceServiceHelper().setStringPrefValue(key: PreferenceKeys.environment, value: environment.name);
+//     await InitSentry().runAppWithSentry(buildProvidersTree(), environment: environment);
+//   }
+
+//   Future<String> getEnvironment() async {
+//     String environment = await PreferenceServiceHelper().getStringPrefValue(key: PreferenceKeys.environment);
+//     return environment;
+//   }
+
+//   Future<void> _initializeApp({required Environment environment}) async {
+//     WidgetsFlutterBinding.ensureInitialized();
+
+    
+//     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+//     SystemChrome.setSystemUIOverlayStyle(
+//       const SystemUiOverlayStyle(
+//         statusBarBrightness: Brightness.light,
+//         statusBarColor: AppColors.white,
+//         statusBarIconBrightness: Brightness.dark,
+//         systemNavigationBarColor: AppColors.white,
+//         systemNavigationBarDividerColor: AppColors.white,
+//         systemNavigationBarIconBrightness: Brightness.dark,
+//       ),
+//     );
+
+//     await PreferenceServiceHelper.init();
+//   }
+// }
