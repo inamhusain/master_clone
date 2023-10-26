aeb5329b8bbcb4a81a910c02e426a779086e4846
inamh_webelight <inamh@webelight.co.in>
2023-10-26 11:30:39 +0530

    latest

---
+++ {{cookiecutter.repo_name}}/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,32 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:catcher/catcher_web_plugin.dart';
+import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:sentry_flutter/sentry_flutter_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  CatcherWebPlugin.registerWith(registrar);
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  SentryFlutterWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
