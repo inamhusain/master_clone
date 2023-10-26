1c7cfdde0660e89189d065b2f6381b5c4c0de195
inamh_webelight <inamh@webelight.co.in>
2023-10-26 18:50:44 +0530

    latest

--- {{cookiecutter.repo_name}}/lib/providers/splash_provider.dart
+++ {{cookiecutter.repo_name}}/lib/providers/splash_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:ls/config/env/app_env.dart';
 import 'package:master_utility/master_utility.dart';
+import 'package:{{cookiecutter.repo_name}}/config/env/app_env.dart';
 import 'package:{{cookiecutter.repo_name}}/helpers/internet_connectivity.dart';
 
 class SplashProvider extends ChangeNotifier {
