0bb1bfb1cd09e79393cdbc97fd15d4121a30c2e2
Inam Husain <70013058+inamhusain@users.noreply.github.com>
2023-12-14 12:22:42 +0530

    Update splash_provider.dart

--- {{cookiecutter.repo_name}}/lib/providers/splash_provider.dart
+++ {{cookiecutter.repo_name}}/lib/providers/splash_provider.dart
@@ -5,13 +5,13 @@ import 'package:{{cookiecutter.repo_name}}/helpers/internet_connectivity.dart';
 
 class SplashProvider extends ChangeNotifier {
   static const int duration_3s = 3;
-  static const int duration_600ms = 3;
+   
 
   void init(context) async {
     InternetConnectivityHelper().init();
     SizeHelper.setMediaQuerySize(context: context);
     dioClient.setConfiguration(AppEnv.baseUrl);
 
-    await Future.delayed(const Duration(seconds: duration_3s, milliseconds: duration_600ms), () async {});
+    await Future.delayed(const Duration(seconds: duration_3s ), () async {});
   }
 }
